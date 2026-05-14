e000a5bcb1beb36c14cdaa7c08db5d8247f1b0b4
8shagrid <halimdirga8@gmail.com>
2026-05-15 00:08:32 +0700

    feat: add landing page links and update favicon assets

---
+++ pad-image.ps1
@@ -0,0 +1,13 @@
+﻿Add-Type -AssemblyName System.Drawing
+$src = [System.Drawing.Image]::FromFile('b:\10_DEVELOPMENT\projects\seraya\assets\img\logo.png')
+$size = [Math]::Max($src.Width, $src.Height)
+$bmp = New-Object System.Drawing.Bitmap $size, $size
+$g = [System.Drawing.Graphics]::FromImage($bmp)
+$g.Clear([System.Drawing.Color]::Transparent)
+$x = ($size - $src.Width) / 2
+$y = ($size - $src.Height) / 2
+$g.DrawImage($src, $x, $y, $src.Width, $src.Height)
+$bmp.Save('b:\10_DEVELOPMENT\projects\seraya\assets\img\favicon.png', [System.Drawing.Imaging.ImageFormat]::Png)
+$g.Dispose()
+$bmp.Dispose()
+$src.Dispose()
